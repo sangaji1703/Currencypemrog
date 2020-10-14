4abf88198bdbcc875f83da96dabad6099f6bc24c
sangaji <61863989+sangaji1703@users.noreply.github.com>
2020-10-14 23:42:45 +0700

    currency-appspemrog

---
+++ currencyapss/README.md
@@ -0,0 +1,7 @@
+1.	Mengapa perlu membuat class `CurrencyController.cs` pada percobaan 4
+-	Agar source code tidak terlalu panjang, tampilan terlihat lebih bersih  dan jika terdapat bug maka akan terlihat jelas
+2.	. Jelaskan kegunaan method `isAllowed()` pada percobaan 
+-	Kegunaan Method isAllowed yaitu untuk mencari karakter yg di masukkan pengguna apakah cocok atau tidak
+3. Jelaskan secara singkat mengenai Regex pada percobaan 4
+- Regex berfungsi untuk memilih angka menggunakan pola tertentu setelah itu akan di teruskan ke perintah selanjutnya
+4. Buatlah class diagramnya pada percobaan 4	
---
+++ currencyapss/currency/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simple_Currency_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CurrencyController currencyController;
+        public MainWindow()
+        {
+            InitializeComponent();
+            currencyController = new CurrencyController();
+        }
+
+        private void hitungButton_Click(object sender, RoutedEventArgs e)
+        {
+            var nominalString = inputTextBox.Text;
+
+            var result = "Inputan tidak valid";
+
+            if(currencyController.isAllowed(nominalString))
+            {
+                result = "Rp. " + currencyController.usdToIdr(nominalString);
+            }
+
+            resultLabel.Content = result;
+            
+        }
+    }
+}
